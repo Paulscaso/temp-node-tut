77f31eb23e9d8053f5fafcb3b649638f89302403
Paul Kimotho <paulkimotho2@gmail.com>
2023-06-03 16:23:57 +0300

    tutorial commit

---
+++ 4-modules.js
@@ -0,0 +1,10 @@
+const names = require("./1-names");
+const sayHi = require("./2-utils");
+const data = require("./2-alternative_flavor");
+require("./3-mind-greenade");
+
+
+
+sayHi("susan");
+sayHi(names.john);
+sayHi(names.peter);
